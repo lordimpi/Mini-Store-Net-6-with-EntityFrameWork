1c4f6e3eec2de18b59d5d867da50d5efcd7a4b58
Santiago Acuña <snt-26@hotmail.com>
2022-07-05 12:46:48 -0500

    Add seeder

--- MiniStore/MiniStoreWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniStoreWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
